de6930c4a01aba84bb11aa82f9925f3f768606ba
JOAB VIVANCO <joab.vj@estudiante.uc.cl>
2025-11-27 11:26:45 -0300

    .

--- docs/script.js
+++ docs/script.js
@@ -41,39 +41,39 @@ const projectContent = [
     },
     { 
         title: 'Descripción del Problema', 
-        content: 'Este proyecto de IMT2200 analiza las tendencias en las enfermedades respiratorias antes y después del impacto de la COVID-19, enfocándose en la infraestructura de salud pública y la respuesta social.',
+        content: 'A',
         // subtitle: 'Objetivos',
         // subcontent: ''
         // imageSrc: "IMAGES/grafico.png"
     },
     { 
         title: 'Datos Utilizados', 
-        content: 'Este proyecto de IMT2200 analiza las tendencias en las enfermedades respiratorias antes y después del impacto de la COVID-19, enfocándose en la infraestructura de salud pública y la respuesta social.',
+        content: 'A',
         // subtitle: 'Objetivos',
         // subcontent: ''
         // imageSrc: "IMAGES/grafico.png"
     },
     { 
         title: 'Metodología', 
-        content: 'Este proyecto de IMT2200 analiza las tendencias en las enfermedades respiratorias antes y después del impacto de la COVID-19, enfocándose en la infraestructura de salud pública y la respuesta social.',
+        content: 'A',
         // subtitle: 'Objetivos',
         // subcontent: ''
         // imageSrc: "IMAGES/grafico.png"
     },
     { 
         title: 'Proceso de ETL / Procesamiento Info', 
-        content: 'Este proyecto de IMT2200 analiza las tendencias en las enfermedades respiratorias antes y después del impacto de la COVID-19, enfocándose en la infraestructura de salud pública y la respuesta social.',
+        content: 'A',
         // subtitle: 'Objetivos',
         // subcontent: ''
         // imageSrc: "IMAGES/grafico.png"
     },
     { 
         title: 'Análisis y Resultados', 
-        content: 'El análisis muestra un claro descenso en las enfermedades respiratorias comunes durante el confinamiento (2020) y un aumento significativo en la sobrecarga del sistema en los años posteriores (2022). Se observó una correlación negativa del -0.75 entre el distanciamiento social y la incidencia de gripe.',
+        content: 'A',
     },
     { 
         title: 'Interpretación', 
-        content: 'Recibimos comentarios valiosos de profesores y expertos en salud pública, lo que llevó a recalcular las tasas ajustadas por población para mejorar la precisión del análisis.',
+        content: 'A',
     },
     { 
         title: 'Links Relevantes', 
